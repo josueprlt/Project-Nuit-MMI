a8270d5d020df84488c82a9eb7b6afe406c38a41
josueprlt <148058543+josueprlt@users.noreply.github.com>
2025-01-09 16:03:55 +0100

    add and fix Total CO2

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
-let totalCookies = 0
+let mails = 0
+let totalMails = 0
+let totalCO2 = 0
 let cookiesPerClick = 1
 let clicksPerSecond = 0
 let isCookieClicked = false
@@ -7,61 +9,79 @@ let isAutoClicked = false
 
 let upgradeCost = 5
 let autoClickerCost = 10
+
 function setup() {
-createCanvas(windowWidth, windowHeight)
+	createCanvas(windowWidth, windowHeight)
 }
 
 function draw() {
-	if(frameCount % 60 == 0) {
-		totalCookies += clicksPerSecond
+	// Ajouter les mails des clics automatiques chaque seconde
+	if (frameCount % 60 == 0) {
+		mails += clicksPerSecond
+		totalMails += clicksPerSecond
+		mailFromCO2(clicksPerSecond)
 	}
-	
+
 	background(220)
 	fill(0)
 	textSize(20)
 	textAlign(CENTER, CENTER)
-	text('Total: ' + totalCookies, width/2, 50)
-	text(cookiesPerClick + ' per click', width/2, 80)
-	text(clicksPerSecond + ' per sec', width/2, 110)
-	if(isCookieClicked) {
+	text('Nombre de mails actuels : ' + mails, width / 2, 50)
+	if (totalCO2 > 1000) {
+		text('Total CO2 évité : ' + totalCO2 + ' kg', width / 2, 110)
+	}
+	else if (totalCO2 > 10000) {
+		text('Total CO2 évité : ' + totalCO2 + ' t', width / 2, 110)
+	}
+	else {
+		text('Total CO2 évité : ' + totalCO2 + ' g', width / 2, 110)
+	}
+	text(cookiesPerClick + ' mails par clic', width / 2, 140)
+	text(clicksPerSecond + ' mails par seconde', width / 2, 170)
+
+	if (isCookieClicked) {
 		fill(255, 0, 0)
 	} else {
 		fill(255)
 	}
-	ellipse(width/2, height/2, 300)
-	if(isUpgradeClicked) {
-		fill(255, 0, 0)
-	} else {
-		fill(255)
-	}
-	if(isAutoClicked) {
-		fill(255, 0, 0)
-	} else {
-		fill(255)
-	}
+	ellipse(width / 2, height / 2, 300)
+
 	fill(0)
-	text('Email', width/2, height/2)
-	text(upgradeCost + ' upgrade', 0.25*width, 0.875*height)
-	text(autoClickerCost + ' autoclicker', 0.75*width, 0.875*height)
+	text(upgradeCost + ' pour augmenter les clics', 0.25 * width, 0.875 * height)
+	text(autoClickerCost + ' pour un clic automatique', 0.75 * width, 0.875 * height)
 }
+
 function mousePressed() {
-	if(dist(mouseX, mouseY, width/2, height/2) <= 100) {
+	if (dist(mouseX, mouseY, width / 2, height / 2) <= 150) {
 		isCookieClicked = true
-		totalCookies += cookiesPerClick
-	} else if(mouseY > 300 && mouseX < width/2 && totalCookies >= upgradeCost) {
+		mails += cookiesPerClick
+		totalMails += cookiesPerClick
+		mailFromCO2(cookiesPerClick)
+	}
+	else if (mouseY > height * 0.75 && mouseX < width / 2 && mails >= upgradeCost) {
 		isUpgradeClicked = true
 		cookiesPerClick++
-		totalCookies -= upgradeCost
+		mails -= upgradeCost
 		upgradeCost = parseInt(upgradeCost * 1.3)
-	} else if(mouseY > 300 && mouseX >= width/2 && totalCookies >= autoClickerCost) {
+	}
+	else if (mouseY > height * 0.75 && mouseX >= width / 2 && mails >= autoClickerCost) {
 		isAutoClicked = true
 		clicksPerSecond++
-		totalCookies -= autoClickerCost
+		mails -= autoClickerCost
 		autoClickerCost = parseInt(autoClickerCost * 1.2)
 	}
 }
+
 function mouseReleased() {
 	isCookieClicked = false
 	isUpgradeClicked = false
 	isAutoClicked = false
+}
+
+function mailFromCO2(newMails) {
+	let CO2Generated = newMails * 4 // 4g par mail
+	totalCO2 += CO2Generated
+}
+
+function grammesToKg(grammes) {
 }
